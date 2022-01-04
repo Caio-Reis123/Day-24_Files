e041b9d4c8b158e7d721cad39e9e523043238ca1
Caio-Reis123 <77801252+Caio-Reis123@users.noreply.github.com>
2022-01-04 18:15:18 -0300

    Complete

--- Code/Mail-Merge-Project-Start/main.py
+++ Code/Mail-Merge-Project-Start/main.py
@@ -1,20 +1,32 @@
+PLACEHOLDER = '[name]'
+
+# CREATE LETTER LIST
 new_letter = []
-with open('Mail-Merge-Project-Start/Input/Names/invited_names.txt') as name_list:
+
+# OPEN 'INVITED_NAMES' FILE AND SPLIT IT
+with open('./Mail-Merge-Project-Start/Input/Names/invited_names.txt') as name_list:
     names = name_list.read().split()
 
+# LOOP THROUGH NAMES, OPEN 'STARTING_LETTER' FILE
     for name in names:
 
-        with open('Mail-Merge-Project-Start/Input/Letters/starting_letter.txt', mode='r') as letters:
+        with open('./Mail-Merge-Project-Start/Input/Letters/starting_letter.txt', mode='r') as letters:
             content = letters.readlines()
-        line = content[0].replace('[name]', name)
+
+# SEPARATE 1ST LINE AND REPLACE THE NAMES, THEN APPEND IT TO NEW_LETTER      
+        line = content[0].replace(PLACEHOLDER, name)
         new_letter.append(line)
 
-        for a in content[1:]:
-            new_letter.append(a)
-        print(new_letter)
+# APPEND THE REMANING LINES OF LETTER TO NEW_LETTER
+        for i in content[1:]:
+            new_letter.append(i)
 
+# CREATE A NEW FILE FOR EACH NAME AND RESET NEW_LETTER 
         with open(name +'_letter.txt', mode='w') as textfile:
 
             for element in new_letter:
                 textfile.write(element)
         new_letter = []
+
+
+
